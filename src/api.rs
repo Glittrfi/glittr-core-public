711642e2a3974caed0c15deba35272dc99e117b9
Irfi M <irfi@localcitizen>
2024-10-22 17:53:19 +0700

    hot-fix: axum shared state

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use super::*;
 use axum::{
     extract::{Path, State},
     http::StatusCode,
@@ -9,19 +10,17 @@ use axum::{
 use serde_json::{json, Value};
 use store::database::{DatabaseError, MESSAGE_PREFIX, TRANSACTION_TO_BLOCK_TX_PREFIX};
 use transaction::message::OpReturnMessage;
-use super::*;
-
 
 // TODO: The database lock could possibly slowing down indexing. Add cache or rate limit for the API.
+#[derive(Clone)]
 pub struct APIState {
     pub database: Arc<Mutex<Database>>,
 }
 pub async fn run_api(database: Arc<Mutex<Database>>) -> Result<(), std::io::Error> {
-    let shared_state = Arc::new(APIState { database });
+    let shared_state = APIState { database };
     let app = Router::new()
         .route("/health", get(health))
         .route("/tx/:txid", get(tx_result))
-        .with_state(shared_state.clone())
         .route("/blocktx/:block/:tx", get(get_block_tx))
         .with_state(shared_state);
     log::info!("API is listening on {}", CONFIG.api_url);
@@ -31,10 +30,9 @@ pub async fn run_api(database: Arc<Mutex<Database>>) -> Result<(), std::io::Erro
 }
 
 async fn tx_result(
-    State(mut state): State<Arc<APIState>>,
+    State(state): State<APIState>,
     Path(txid): Path<String>,
 ) -> Result<Json<Value>, StatusCode> {
-    let state = Arc::get_mut(&mut state).unwrap();
     let blocktx: Result<BlockTxTuple, DatabaseError> = state
         .database
         .lock()
@@ -65,11 +63,10 @@ async fn tx_result(
 }
 
 async fn get_block_tx(
-    State(mut state): State<Arc<APIState>>,
+    State(state): State<APIState>,
     Path(block): Path<u64>,
     Path(tx): Path<u32>,
 ) -> Result<Json<Value>, StatusCode> {
-    let state = Arc::get_mut(&mut state).unwrap();
     let message: Result<OpReturnMessage, DatabaseError> = state
         .database
         .lock()
