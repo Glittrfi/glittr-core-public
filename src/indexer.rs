2a03e87683fe488fab0dd2fd8340202be4eb0f18
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-10-22 12:33:16 +0700

    update: last_indexed_block to optional

--- src/indexer.rs
+++ src/indexer.rs
@@ -8,7 +8,7 @@ use transaction::message::OpReturnMessage;
 pub struct Indexer {
     rpc: Client,
     database: Database,
-    last_indexed_block: u64,
+    last_indexed_block: Option<u64>,
 }
 
 impl Indexer {
@@ -23,7 +23,7 @@ impl Indexer {
 
         let database = Database::new();
 
-        let last_indexed_block: u64 = database.get(INDEXER_LAST_BLOCK_PREFIX, "").unwrap_or(0);
+        let last_indexed_block: Option<u64> = database.get(INDEXER_LAST_BLOCK_PREFIX, "");
 
         Ok(Indexer {
             last_indexed_block, // Todo: change this to rocksdb
@@ -36,12 +36,19 @@ impl Indexer {
         loop {
             let current_block_tip = self.rpc.get_block_count()?;
 
-            while self.last_indexed_block < current_block_tip {
-                let block_height = self.last_indexed_block + 1;
+            while self.last_indexed_block.is_none()
+                || self.last_indexed_block.unwrap() < current_block_tip
+            {
+                let block_height = match self.last_indexed_block {
+                    Some(value) => value + 1,
+                    None => 0,
+                };
+
                 let block_hash = self.rpc.get_block_hash(block_height)?;
                 let block = self.rpc.get_block(&block_hash)?;
 
                 log::info!("Indexing block {}: {}", block_height, block_hash);
+                println!("Indexing block {}: {}", block_height, block_hash);
 
                 for (pos, tx) in block.txdata.iter().enumerate() {
                     // run modules here
@@ -60,11 +67,11 @@ impl Indexer {
                     });
                 }
 
-                self.last_indexed_block += 1;
+                self.last_indexed_block = Some(block_height);
             }
 
             self.database
-                .put(INDEXER_LAST_BLOCK_PREFIX, "", self.last_indexed_block)?;
+                .put(INDEXER_LAST_BLOCK_PREFIX, "", self.last_indexed_block.unwrap())?;
 
             thread::sleep(Duration::from_secs(10));
         }
