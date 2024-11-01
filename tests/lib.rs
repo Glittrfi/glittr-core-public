58bc634d37f48cb580d0ccaa3135315df0340849
Irfi M <irfi@localcitizen>
2024-11-01 18:37:08 +0700

    test: add 2 sample tests for wrapped btc (oracle, and one-way purchase)

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,3 +1,4 @@
+use axum::routing::get;
 use bitcoin::{
     hashes::{sha256, Hash},
     key::{rand, Keypair, Secp256k1},
@@ -356,9 +357,23 @@ async fn test_raw_btc_to_glittr_asset_burn() {
     ctx.drop().await;
 }
 
-// test_raw_btc_to_glittr_asset_purchase e.g. raw btc to wbtc by purchase
+///Example: most basic gBTC implementation
+/// {
+///     TxType: Contract,
+///     SimpleAsset:{
+///         supplyCap: 21000000,
+///         Divisibility: 100000000,
+///         Livetime: -10
+///     },
+///     Purchase:{
+///         inputAsset: BTC,
+///         payTo: pubkey,
+///         Ratio: 1,
+///     }
+///     }
+/// test_raw_btc_to_glittr_asset_purchase e.g. raw btc to wbtc by purchase
 #[tokio::test]
-async fn test_raw_btc_to_glittr_asset_purchase() {
+async fn test_raw_btc_to_glittr_asset_purchase_gbtc() {
     let mut ctx = TestContext::new().await;
 
     let contract_treasury = get_bitcoin_address();
@@ -366,8 +381,8 @@ async fn test_raw_btc_to_glittr_asset_purchase() {
         tx_type: TxType::ContractCreation {
             contract_type: ContractType::Asset(AssetContract {
                 asset: SimpleAsset {
-                    supply_cap: None,
-                    divisibility: 18,
+                    supply_cap: Some(U128(21_000_000 * 10u128.pow(8))),
+                    divisibility: 8,
                     live_time: 0,
                 },
                 distribution_schemes: DistributionSchemes {
@@ -554,6 +569,171 @@ async fn test_raw_btc_to_glittr_asset_burn_oracle() {
         outputs: 1,
         p2tr: false,
         recipient: Some(minter_address),
+    });
+
+    ctx.core.mine_blocks(1);
+
+    let mint_block_tx = BlockTx {
+        block: height + 1,
+        tx: 1,
+    };
+
+    start_indexer(Arc::clone(&ctx.indexer)).await;
+    ctx.get_and_verify_message_outcome(contract_id).await;
+    let mint_outcome = ctx.get_and_verify_message_outcome(mint_block_tx).await;
+    assert!(mint_outcome.flaw.is_none(), "{:?}", mint_outcome.flaw);
+
+    let tx = ctx.get_transaction_from_block_tx(mint_block_tx).unwrap();
+
+    let asset_list: Result<Vec<(String, AssetList)>, DatabaseError> = ctx
+        .indexer
+        .lock()
+        .await
+        .database
+        .lock()
+        .await
+        .expensive_find_by_prefix(ASSET_LIST_PREFIX);
+    let asset_lists = asset_list.expect("asset list should exist");
+
+    for (k, v) in &asset_lists {
+        println!("Mint output: {}: {:?}", k, v);
+    }
+
+    let outpoint_str = asset_lists[0].0.clone();
+    let out_value = *asset_lists[0].1.list.get(&contract_id.to_str()).unwrap();
+
+    assert!(out_value == oracle_out_value);
+    assert!(
+        outpoint_str
+            == format!(
+                "{}:{}",
+                "asset_list",
+                Outpoint {
+                    txid: tx.compute_txid().to_string(),
+                    vout: 1
+                }
+                .to_string()
+            )
+    );
+
+    ctx.drop().await;
+}
+
+
+/// Example: most basic Hermetica implementation
+/// {
+/// TxType: contract,
+/// simpleAsset:{
+/// 	supplyCap: -1,
+/// 	Divisibility: 100,
+/// 	Livetime: -10,
+/// },
+/// Purchase:{
+/// 	inputAsset: BTC,
+/// 	payTo: pubkey,
+/// 	Ratio: {
+/// 		Oracle: {
+/// 	        oracleKey: pubKey,
+/// 	        Args:{
+///               satsPerDollar: int,
+///               purchaseValue: float,
+///               BLOCKHEIGHT: {AllowedBlockSlippage: -5},
+///             },
+///        },
+///     },
+/// },
+/// }
+/// 
+#[tokio::test]
+async fn test_raw_btc_to_glittr_asset_oracle_purchase() {
+    let mut ctx = TestContext::new().await;
+
+    let secp = Secp256k1::new();
+    let oracle_keypair = Keypair::new(&secp, &mut rand::thread_rng());
+    let oracle_xonly = XOnlyPublicKey::from_keypair(&oracle_keypair);
+
+    let treasury_address = get_bitcoin_address();
+    let contract_message = OpReturnMessage {
+        tx_type: TxType::ContractCreation {
+            contract_type: ContractType::Asset(AssetContract {
+                asset: SimpleAsset {
+                    supply_cap: Some(U128(21_000_000 * 10u128.pow(8))),
+                    divisibility: 8,
+                    live_time: 0,
+                },
+                distribution_schemes: DistributionSchemes {
+                    purchase: Some(PurchaseBurnSwap {
+                        input_asset: InputAsset::RawBTC,
+                        transfer_scheme: TransferScheme::Purchase(treasury_address.to_string()),
+                        transfer_ratio_type: TransferRatioType::Oracle {
+                            pubkey: oracle_xonly.0.serialize().to_vec(),
+                            setting: OracleSetting {
+                                asset_id: Some("btc".to_string()),
+                                block_height_slippage: 5,
+                            },
+                        },
+                    }),
+                    preallocated: None,
+                    free_mint: None,
+                },
+            }),
+        },
+    };
+
+    let contract_id = ctx.build_and_mine_message(&contract_message).await;
+
+
+    // prepare btc
+    let bitcoin_value = 50000;
+    let fee = 100;
+    let oracle_out_value = 1000;
+
+    ctx.core.mine_blocks_with_subsidy(1, bitcoin_value);
+    let height = ctx.core.height();
+
+    let input_txid = ctx.core.tx((height - 1) as usize, 0).compute_txid();
+    let oracle_message = OracleMessage {
+        input_outpoint: OutPoint {
+            txid: input_txid,
+            vout: 0,
+        },
+        min_in_value: (bitcoin_value - 1000) as u128,
+        out_value: oracle_out_value,
+        asset_id: Some("btc".to_string()),
+        block_height: height,
+    };
+
+    let secp: Secp256k1<secp256k1::All> = Secp256k1::new();
+    let msg = Message::from_digest_slice(
+        sha256::Hash::hash(serde_json::to_string(&oracle_message).unwrap().as_bytes())
+            .as_byte_array(),
+    )
+    .unwrap();
+
+    let signature = secp.sign_schnorr(&msg, &oracle_keypair);
+
+    let mint_message = OpReturnMessage {
+        tx_type: TxType::ContractCall {
+            contract: contract_id.to_tuple(),
+            call_type: CallType::Mint(MintOption {
+                pointer: 1,
+                oracle_message: Some(OracleMessageSigned {
+                    signature: signature.serialize().to_vec(),
+                    message: oracle_message.clone(),
+                }),
+            }),
+        },
+    };
+    ctx.core.broadcast_tx(TransactionTemplate {
+        fee: 0,
+        inputs: &[((height - 1) as usize, 0, 0, Witness::new())],
+        op_return: Some(mint_message.into_script()),
+        op_return_index: Some(0),
+        op_return_value: Some(0),
+        output_values: &[bitcoin_value - fee],
+        outputs: 1,
+        p2tr: false,
+        recipient: Some(treasury_address),
     });
 
     ctx.core.mine_blocks(1);
