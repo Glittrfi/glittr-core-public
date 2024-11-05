0ce2c98b5587989d8c1f4a1c3f0338a662a85950
Irfi M <irfi@localcitizen>
2024-11-05 18:55:41 +0700

    review: fixes

--- src/transaction/asset_contract.rs
+++ src/transaction/asset_contract.rs
@@ -10,8 +10,6 @@ use super::*;
 #[serde(rename_all = "snake_case")]
 pub struct FreeMint {
     pub supply_cap: Option<U128>,
-    // TODO change the type to u128, need to check the serialization and deserialization since JSON
-    // has a MAX number limitation.
     pub amount_per_mint: U128,
 }
 
